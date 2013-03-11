51dce7b45a3389f4c8e6c1cd07770c8d7cd42b32
f.nasim <f.nasim01@gmail.com>
2013-03-11 15:51:51 +0600

    Added gl-install-commit.php

---
+++ gl-install-commit.php
@@ -0,0 +1,66 @@
+<html>
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "includes/styles/global.css">
+  </head>
+  
+  <body bgcolor = "#B8B894">
+    <table width = "990" cellpadding = "15">
+      <tr>
+        <td width = "25%"></td>
+		
+        <td>
+          <h2 align = "center">Glossary Manager<br> Installation</h2>
+        </td>
+
+        <td width = "25%"></td>
+      </tr>
+
+	  <tr>
+	    <td width = "25%"></td>
+
+		<td bgcolor = "#D6D6C2">
+	      <?php
+            # establishes database connection
+            #
+            $con = mysql_connect("localhost", $_POST['db_user'], $_POST['db_pass']);
+
+            if(!$con){
+              die('Could not connect to database: ' . mysql_error());
+            }
+
+            mysql_select_db($_POST['db_name'], $con);
+
+            $query = "CREATE TABLE `glossary_entries` ( `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_roman_ci NOT NULL, `content` varchar(2000) CHARACTER SET utf8 COLLATE utf8_roman_ci NOT NULL, `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`title`)) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+            if(mysql_query($query)){
+              echo "<p class = \"content\"><b>Table is created.</b></p>";
+            }
+            else{
+              echo "<p class = \"content\"><b>Could not create table.</b></p>";
+	          exit;
+            }
+
+            $file_names = array('gl-add-new-entry.php', 'gl-save-entry.php', 'gl-del-entry.php', 'gl-view-entries.php');
+
+            # updates database constants in all files
+            #
+            foreach ($file_names as $file_name){
+              $file_content = file_get_contents($file_name);
+              $file_content = preg_replace('/db_name/', $_POST['db_name'], $file_content);
+              $file_content = preg_replace('/db_user/', $_POST['db_user'], $file_content);
+              $file_content = preg_replace('/db_pass/', $_POST['db_pass'], $file_content);
+              file_put_contents($file_name, $file_content);
+		
+		      echo "<p class = \"content\"><b>Updated file:</b> <i>" . $file_name . "</i></p>";
+            }
+	  
+	        echo "<p class = \"content-header\"><b>You are done!</b></p>";
+	        echo "<p class = \"content-header\">Continue to <a href = \"index.php\">Home</a></p>";
+          ?>
+	    </td>
+	  
+	    <td width = "25%"></td>
+	  </tr>
+	</table>
+  </body>
+</html>
