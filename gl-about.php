ee15c29e29f1ae14498871b2d2d6b0fe1a5402a9
f.nasim <f.nasim01@gmail.com>
2013-03-11 22:12:29 +0600

    Fixed indentation

--- gl-about.php
+++ gl-about.php
@@ -1,1 +1,52 @@
-<html>  <head>    <link rel = "stylesheet" type = "text/css" href = "includes/styles/global.css">		<title>About</title>  </head>    <body bgcolor = "#B8B894">    <table align = "center" width = "990" cellpadding = "15">	  <tr>	  </tr>	  	  <tr>		<td width = "20%"></td>				<td>	      <h1 align = "center">Glossary Manager</h1>		</td>				<td width = "20%"></td>	  </tr>	  	  <tr>	    <td width = "20%"></td>				<td bgcolor = "#D6D6C2">		  <p class = "content">		    <i>Created by</i> Farhan Nasim.		  </p>		  		  <p class = "content">		    Based on:<br>		  </p>		  		  <p><font color = "#800000"><b>PHP</b></font><br><i>As server side language.</i></p>		  <p><font color = "#800000"><b>MySQL</b></font><br><i>As database manager.</i></p>		  <p><font color = "#800000"><b>JavaScript</b></font><br><i>As client side language.</i></p>		  <p><font color = "#800000"><b>PHP MarkDown</b></font><br><i>As text formatter.</i></p>		  <p><font color = "#800000"><b>MathJAX</b></font><br><i>As mathematical notation formatter.</i></p>		</td>				<td width = "20%"></td>	  </tr>      <tr>        <td colspan = "3">          <?php include 'gl-footer.php';?>        </td>      </tr>	</table>  </body></html>
+<html>
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "includes/styles/global.css">
+
+    <title>About</title>
+  </head>
+
+  <body bgcolor = "#B8B894">
+    <table align = "center" width = "990" cellpadding = "15">
+      <tr>
+      </tr>
+
+      <tr>
+        <td width = "20%"></td>
+
+        <td>
+          <h1 align = "center">Glossary Manager</h1>
+        </td>
+
+        <td width = "20%"></td>
+      </tr>
+
+      <tr>
+        <td width = "20%"></td>
+
+        <td bgcolor = "#D6D6C2">
+          <p class = "content">
+            <i>Created by</i> Farhan Nasim.
+          </p>
+
+          <p class = "content">
+            Based on:<br>
+          </p>
+
+          <p><font color = "#800000"><b>PHP</b></font><br><i>As server side language.</i></p>
+          <p><font color = "#800000"><b>MySQL</b></font><br><i>As database manager.</i></p>
+          <p><font color = "#800000"><b>JavaScript</b></font><br><i>As client side language.</i></p>
+          <p><font color = "#800000"><b>PHP MarkDown</b></font><br><i>As text formatter.</i></p>
+          <p><font color = "#800000"><b>MathJAX</b></font><br><i>As mathematical notation formatter.</i></p>
+        </td>
+
+        <td width = "20%"></td>
+      </tr>
+
+      <tr>
+        <td colspan = "3">
+          <?php include 'gl-footer.php';?>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
