336192cc166c7292b8cd40c837bba9d3791fa83b
f.nasim <f.nasim01@gmail.com>
2013-03-11 15:54:35 +0600

    Added index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <link rel = "stylesheet" type = "text/css" href = "includes/styles/global.css">
+
+	<title>Glossary Manager</title>
+  </head>
+  
+  <body bgcolor = "#B8B894">
+    <table width = "990">
+	  <tr>
+	  </tr>
+	  
+	  <tr>
+		<td width = "15%"></td>
+		
+		<td>
+	      <h1 align = "center">Glossary Manager</h1>
+		</td>
+		
+		<td width = "15%"></td>
+	  </tr>
+	  
+	  <tr>
+	    <td width = "15%"></td>
+		
+		<td>
+		  <p class= "header">
+            <a href = "gl-view-entries.php?start=A">View Glossary</a> <br/>
+	        <a href = "gl-add-new-entry.php">Add New Entry</a>
+		  </p>
+		</td>
+		
+        <td width = "15%"></td>
+
+      </tr>
+	  
+      <tr>
+        <td colspan = "3">
+          <?php include 'gl-footer.php';?>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
