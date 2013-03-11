c2fa5a9341f4de56c0e6c37c77b45ad70143d1dd
f.nasim <f.nasim01@gmail.com>
2013-03-11 15:48:49 +0600

    Added gl-footer.php

---
+++ gl-footer.php
@@ -0,0 +1,9 @@
+<p class = "content" align = "center">
+  <br><br>
+  <a href = "index.php">Home</a> &nbsp; | &nbsp;
+  <a href = "gl-view-entries.php?start=A">All Entries</a> &nbsp; | &nbsp;
+  <a href = "gl-add-new-entry.php">Add New Entry</a> &nbsp; | &nbsp;
+  <a href = "gl-about.php">About</a>
+  <br><br>
+  <font face = "sans-serif" size = "2">Best view resolution: 1024 X 768</font>
+</p>
